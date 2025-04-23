29eccad28b7acf63eea6cea074f14aa82e691cba
Bas Kaal <3askaal@gmail.com>
2025-04-23 23:38:57 +0200

    refactor config hook to rerender when config changes, add support for selecting font variants in typography experiment, clean up

--- src/app/constants/projects.ts
+++ src/app/constants/projects.ts
@@ -1,4 +1,4 @@
 export const PROJECTS = [
   { label: 'Crystal Star', path: 'crystal-star', color: '#FF0B55' },
-  { label: 'Typography', path: 'typo', color: '#444' }
+  { label: 'Typography', path: 'typo', color: '#555555' }
 ]
--- src/app/projects/typo/page.tsx
+++ src/app/projects/typo/page.tsx
@@ -3,51 +3,61 @@
 import { useEffect, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { load } from 'opentype.js'
-import { useConfig, makeSeparator, makeButton, makeFolder } from '@/app/hooks/useConfig'
+import { mapValues } from 'lodash'
+import { useConfig, makeSeparator, makeButton } from '@/app/hooks/useConfig'
 import { Box } from '@/app/components/Box'
 import { PROJECTS } from '@/app/constants/projects'
 import { Scene } from './Scene'
 import { convertFont, getFontOptions } from './helpers/fonts'
 
 const Page = () => {
-  const [fontOptions, setFontOptions] = useState<any>([])
+  const [fonts, setFonts] = useState<any>(null)
+  const [fontVariants, setFontVariants] = useState<any>(null)
   const [fontData, setFontData] = useState<any>(null)
 
+  const fontFamilies = mapValues(fonts, (value, key) => key)
+
   useEffect(() => {
-    (async () => {
-      setFontOptions(await getFontOptions())
-    })()
+    (async () => { setFonts(await getFontOptions()) })()
   }, [])
 
   const { config, reset } = useConfig({
-    ...makeFolder('config', {
-      ...(fontOptions.length && {
-        fonts: { type: 'options', label: 'font', options: fontOptions, value: fontOptions[0].value }
-      }),
-      color: { value: PROJECTS[1].color },
-      bgColor: { value: '#1c1c1c' },
-      amount: { value: 10, min: 5, max: 100, step: 1 },
-      offset: { value: 40, min: -100, max: 100, step: 1 },
-      posY: { value: -5, min: -50, max: 50, step: 1 },
-      ...makeSeparator(),
-      objectRotation: { label: 'object ⟳', value: 5, min: -100, max: 100, step: 1 },
-      zoom: { value: 30, min: 1, max: 500, step: 10 },
-      ...makeSeparator(),
-      lights: { value: true },
-      wireframe: { value: true },
-      ...makeSeparator(),
-      ...makeButton('reset', () => reset())
-    })
+    ...(fontFamilies && {
+      fontFamily: { value: Object.values(fontFamilies)[0] || '', options: fontFamilies }
+    }),
+    ...(fontVariants && {
+      fontVariant: { value: Object.values(fontVariants)[0], options: fontVariants }
+    }),
+    color: { value: PROJECTS[1].color },
+    bgColor: { value: '#1c1c1c' },
+    amount: { value: 10, min: 5, max: 100, step: 1 },
+    offset: { value: 25, min: -100, max: 100, step: 1 },
+    posY: { value: -5, min: -50, max: 50, step: 1 },
+    ...makeSeparator(),
+    objectRotation: { label: 'object ⟳', value: 5, min: -100, max: 100, step: 1 },
+    zoom: { value: 50, min: 1, max: 500, step: 10 },
+    ...makeSeparator(),
+    lights: { value: true },
+    wireframe: { value: true },
+    ...makeSeparator(),
+    ...makeButton('reset', () => reset())
   })
 
   useEffect(() => {
-    if (!config.font) return
+    if (!fonts) return
+    if (!config.fontFamily) return
+
+    setFontVariants(fonts[config.fontFamily])
+  }, [fonts, config?.fontFamily])
+
+  useEffect(() => {
+    if (!config.fontVariant) return
 
     (async () => {
-      const font = await load(config.font as unknown as string)
+      const font = await load(config.fontVariant)
       setFontData(convertFont(font))
     })()
-  }, [config])
+  }, [config?.fontVariant])
 
   return (
     <Box
